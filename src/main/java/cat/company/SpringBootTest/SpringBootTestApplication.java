471461e5910a6b8c0dd8045afe2fc24eacd557e5
Carles Company Soler <carlescs@gmail.com>
2022-10-30 19:58:40 +0100

    Initial commit

---
+++ src/main/java/cat/company/SpringBootTest/SpringBootTestApplication.java
@@ -0,0 +1,13 @@
+package cat.company.SpringBootTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/company/SpringBootTest/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.company.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
