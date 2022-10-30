33a3aa7bcac64565a1503e2361f97e746a560e39
Carles Company Soler <carlescs@gmail.com>
2022-10-30 20:25:37 +0100

    First Thymeleaf template

---
+++ src/main/java/cat/company/SpringBootTest/Home/HomeController.java
@@ -0,0 +1,15 @@
+package cat.company.SpringBootTest.Home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller("/")
+public class HomeController {
+    @RequestMapping("/")
+    public String Home(Model model,@RequestParam(name = "name",defaultValue = "world")String name){
+        model.addAttribute("name", name);
+        return "home";
+    }
+}
--- src/main/java/cat/company/SpringBootTest/SpringBootTestApplication.java
+++ src/main/java/cat/company/SpringBootTest/SpringBootTestApplication.java
@@ -1,10 +1,38 @@
 package cat.company.SpringBootTest;
 
+import javax.servlet.ServletContext;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @SpringBootApplication
 public class SpringBootTestApplication {
+
+	private final TemplateEngine templateEngine;
+
+	public SpringBootTestApplication(final ServletContext servletContext) {
+		super();
+		ServletContextTemplateResolver templateResolver = 
+                new ServletContextTemplateResolver(servletContext);
+        
+        // HTML is the default mode, but we set it anyway for better understanding of code
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        // This will convert "home" to "/WEB-INF/templates/home.html"
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        // Template cache TTL=1h. If not set, entries would be cached until expelled
+        templateResolver.setCacheTTLMs(Long.valueOf(3600000L));
+        
+        // Cache is set to true by default. Set to false if you want templates to
+        // be automatically updated when modified.
+        templateResolver.setCacheable(true);
+        
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTestApplication.class, args);
