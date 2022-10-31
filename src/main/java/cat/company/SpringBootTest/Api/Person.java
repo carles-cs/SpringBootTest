f570eb72adf3e0cb7ae727b859abeae3282949d1
Carles Company Soler <carlescs@gmail.com>
2022-10-31 14:09:38 +0100

    Add lombok

--- src/main/java/cat/company/SpringBootTest/Api/Person.java
+++ src/main/java/cat/company/SpringBootTest/Api/Person.java
@@ -1,25 +1,13 @@
 package cat.company.SpringBootTest.Api;
 
+import lombok.Data;
+
+@Data
 public class Person {
 
     private String name;
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     private String surname;
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
 
     public Person(String name, String surname) {
         this.name = name;
